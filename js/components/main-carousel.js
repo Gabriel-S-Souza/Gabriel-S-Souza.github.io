ec133fcd9062b0df07515fae1b3b6603bff08e95
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2021-12-05 19:21:08 -0300

    add da seção contatos e do footer

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,14 +1,14 @@
 const buttonRight = window.document.querySelector('.button-arrow.-right');
 const buttonLeft = window.document.querySelector('.button-arrow.-left');
 const itensEsconderFinal = window.document.querySelector('.itensEsconderFinal');
-let pixels = 0
+let pixels = 0;
 
 buttonRight.addEventListener('click', function() {
-    pixels = pixels + 100
+    pixels = pixels + 100;
     itensEsconderFinal.style = `transform: translateX(${pixels}px)`;
 });
 
 buttonLeft.addEventListener('click', function() {
-    pixels = pixels - 100
+    pixels = pixels - 100;
     itensEsconderFinal.style = `transform: translateX(${pixels}px)`;
 });
